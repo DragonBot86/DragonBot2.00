39f150636bca3256482590ca063024a0549e6e4f
DragonBot86 <93496266+DragonBot86@users.noreply.github.com>
2021-12-14 11:49:41 -0600

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,7 +1,7 @@
 const help = (prefix) => {
 	return `
 
-⌜𝘿𝙧𝙖𝙜𝙤𝙣𝘽𝙤𝙩 𝙗𝙮 𝘿𝙧𝙖𝙜𝙤𝙣⌟  
+🍒⌜𝘿𝙧𝙖𝙜𝙤𝙣𝘽𝙤𝙩 𝙗𝙮 𝘿𝙧𝙖𝙜𝙤𝙣⌟🍒 
 
 ◉ *INFORMACION*
    ○ Comando: ⌜ ${prefix} ⌟
@@ -20,9 +20,9 @@ Level para tus grupos
 ○ ${prefix}antimenu
 Su nombre lo dice todo (antilink)
 ○ ${prefix}otak
-Monas chinas🥶
+Monas chinas
 ○ ${prefix}shantera
-Interactua con el bot
+Interactua con el bot🍒
 
 ◉ *NUEVOS MENUS*
 ○ ${prefix}juegos
@@ -32,64 +32,64 @@ Descargar musica y videos
 ○ ${prefix}version
 Conoce la versión de tu bot
 ○ ${prefix}welmenu
-Comando de bienvenida solo grupos
+Comando de bienvenida solo grupos🍒
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *RESUELVE TUS DUDAS*
   ║
-  ╠☘️ ○ ${prefix}creador
-  ╚☘️ Dudas o problemas aqui
+  ╠➥🔥 ○ ${prefix}creador
+  ╚➥🔥 Dudas o problemas aqui
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *CREAR STICKERS*
   ║
-  ╠☘️ ○ ${prefix}sticker
-  ╠☘️ ○ ${prefix}attp
-  ╠☘️ Mas un texto corto
-  ╠☘️ ○ ${prefix}stickergif
-  ╚☘️ 6 segundos de video
+  ╠➥🔥 ○ ${prefix}sticker
+  ╠➥🔥 ○ ${prefix}attp
+  ╠➥🔥 Mas un texto corto
+  ╠➥🔥 ○ ${prefix}stickergif
+  ╚➥🔥 6 segundos de video
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *CONVERTIDORES*
   ║
-  ╠☘️ ○ ${prefix}toimg
-  ╠☘️ De sticker a JPG
-  ╠☘️ ○ ${prefix}tomp3
-  ╚☘️ De video a MP3
+  ╠➥🔥 ○ ${prefix}toimg
+  ╠➥🔥 De sticker a JPG
+  ╠➥🔥 ○ ${prefix}tomp3
+  ╚➥🔥 De video a MP3
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *AUDIO*
   ║
-  ╠☘️ ○ ${prefix}idioma
-  ╚☘️ ○ ${prefix}tts es (mas texto)
+  ╠➥🔥 ○ ${prefix}idioma
+  ╚➥🔥 ○ ${prefix}tts es (mas texto)
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *OTROS*
   ║
-  ╠☘️○ ${prefix}wame
-  ╠☘️ Link de Whatsapp
-  ╠☘️ ○ ${prefix}qrcode
-  ╚☘️ Coloca un texto
+  ╠➥🔥○ ${prefix}wame
+  ╠➥🔥 Link de Whatsapp
+  ╠➥🔥 ○ ${prefix}qrcode
+  ╚➥🔥 Coloca un texto
 •°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
 ◉ *GRUPOS*
   ║
-  ╠☘️ ○ ${prefix}unir
+  ╠➥🔥 ○ ${prefix}unir
   ╠ Unir a una persona al grupo
-  ╠☘️ ○ ${prefix}foto
-  ╠☘️ Cambiar foto del grupo
-  ╠☘️ ○ ${prefix}name
-  ╠☘️ Cambiar nombre del grupo
-  ╠☘️ ○ ${prefix}des
-  ╠☘️ Cambiar descripcion del grupo
-  ╠☘️ ○ ${prefix}cerrar
-  ╠☘️ Cerrar el grupo solo admins
-  ╠☘️ ○ ${prefix}abrir
-  ╠☘️ Abrir grupo solo admins
-  ╠☘️ ○ ${prefix}promote
-  ╠☘️ Dar admin a un miembro
-  ╠☘️ ○ ${prefix}demote
-  ╠☘️ Quitar el admin
-  ╠☘️ ○ ${prefix}linkgc
-  ╠☘️ Link del grupo
-  ╠☘️ ○ ${prefix}dioses
+  ╠➥🔥 ○ ${prefix}foto
+  ╠➥🔥 Cambiar foto del grupo
+  ╠➥🔥 ○ ${prefix}name
+  ╠➥🔥 Cambiar nombre del grupo
+  ╠➥🔥 ○ ${prefix}des
+  ╠➥🔥 Cambiar descripcion del grupo
+  ╠➥🔥 ○ ${prefix}cerrar
+  ╠➥🔥 Cerrar el grupo solo admins
+  ╠➥🔥 ○ ${prefix}abrir
+  ╠➥🔥 Abrir grupo solo admins
+  ╠➥🔥 ○ ${prefix}promote
+  ╠➥🔥 Dar admin a un miembro
+  ╠➥🔥 ○ ${prefix}demote
+  ╠➥🔥 Quitar el admin
+  ╠➥🔥 ○ ${prefix}linkgc
+  ╠➥🔥 Link del grupo
+  ╠➥🔥 ○ ${prefix}dioses
   ╠ Nombra a los admins del grupo
-  ╠☘️ ○ ${prefix}todos
-  ╚☘️ Nombra a todos los del grupo
+  ╠➥🔥 ○ ${prefix}todos
+  ╚➥🔥 Nombra a todos los del grupo
 
 Para usar estas funciones el bot necesita admin
   
